d267884ee21358db89b5758be7a0054b7f8ee7b9
wushiqing <wsq1375868809@163.com>
2023-08-17 15:13:56 +0800

    init: init

---
+++ api/index.ts
@@ -0,0 +1,10 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default (request: VercelRequest, response: VercelResponse) => {
+  const { name } = request.query;
+  // response.setHeader("Content-Type", "application/json");
+  console.log("xxxxxxxxxxxxxxxxxxxx");
+  response
+    .status(200)
+    .send('{"msg_type":"text","content":{"text":"request example"}}');
+};
