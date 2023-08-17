4744475f54f2466c38ff41bd566a2bb970055970
wushiqing <wsq1375868809@163.com>
2023-08-17 15:28:31 +0800

    log: request's body

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,7 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 export default (request: VercelRequest, response: VercelResponse) => {
   const { name } = request.query;
   // response.setHeader("Content-Type", "application/json");
-  console.log("xxxxxxxxxxxxxxxxxxxx");
+  console.log("xxxxxxxxxxxxxxxxxxxx", request.body);
   response
     .status(200)
     .send('{"msg_type":"text","content":{"text":"request example"}}');
